3ba1af02ae47566dbf0363154cb9540e10d984fe
rezamohdev <mohammadireza29@gmail.com>
2024-11-22 21:05:20 -0500

    update: javascript

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-var todoListBtnCheck = document.getElementById("toDoList_1");
+var todoListBtnCheck = document.getElementById("card_add_btn");
 todoListBtnCheck.addEventListener("click", (e) => {
   e.preventDefault();
-  window.alert("clicked");
   todoListBtnCheck.style.textDecorationStyle = "strike";
 });
+if ("content" in document.createElement("template")) {
+  // template support verification
+  console.log("✔️ creating template is supported by your browser!");
+
+  const cardList = document.querySelector(".card__list");
+  const listTemplate = document.querySelector("#itemTemplate");
+  const clone = listTemplate.content.cloneNode(true);
+
+  todoListBtnCheck.addEventListener("click", (e) => {
+    console.log("clicked");
+    cardList.appendChild(clone);
+  });
+}
